61b65310863d8fa1e817e73c6fd571ab5982eb17
SOO <122779637+Sooyong97@users.noreply.github.com>
2024-10-15 23:34:24 +0900

    [level 1] Title: 덧칠하기, Time: 2681.23 ms, Memory: 99.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++){
+            for (int j = 0; j < section.length; j++){
+                if (i == section[j]){
+                    answer += 1;
+                    i += m;
+                }
+            }
+        }
+        return answer;
+    }
+}
