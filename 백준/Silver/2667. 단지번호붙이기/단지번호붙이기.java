83f03344246c2491f9ef41fa16c5119cedc66522
SOO <122779637+Sooyong97@users.noreply.github.com>
2024-12-05 22:00:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int count;
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+
+            int[] cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny] && map[nx][ny] == 1) {
+                    q.add(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    count++;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    count = 1;
+                    bfs(i, j);
+                    answer.add(count);
+                }
+            }
+        }
+
+        Collections.sort(answer);
+        System.out.println(answer.size());
+        for (Integer nums : answer) {
+            System.out.println(nums);
+        }
+
+    }
+}
