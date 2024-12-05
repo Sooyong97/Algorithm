f159a550cd3824721f3b49b3a8f5766a2abfbf2b
SOO <122779637+Sooyong97@users.noreply.github.com>
2024-12-05 21:01:32 +0900

    [Silver II] Title: 유기농 배추, Time: 324 ms, Memory: 31228 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Main {
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int M, N;
+
+    private static void bfs(int x, int y, int[][] grid, boolean[][] visited) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 & ny >= 0 && nx < M && ny < N && grid[nx][ny] == 1 && !visited[nx][ny]) {
+                    q.add(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            M = sc.nextInt();
+            N = sc.nextInt();
+            int C = sc.nextInt();
+
+            int[][] grid = new int[M][N];
+            boolean[][] visited = new boolean[M][N];
+
+            for (int j = 0; j < C; j++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                grid[x][y] = 1;
+            }
+
+            int count = 0;
+
+            for (int j = 0; j < M; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (grid[j][k] == 1 && !visited[j][k]) {
+                        bfs(j, k, grid, visited);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+
+        }
+    }
+}
