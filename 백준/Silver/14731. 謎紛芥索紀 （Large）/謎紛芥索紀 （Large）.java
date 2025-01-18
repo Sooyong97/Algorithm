b81859426981cd297e699b144e7f68e557cc13dc
SOO <122779637+Sooyong97@users.noreply.github.com>
2025-01-18 15:30:32 +0900

    [Silver I] Title: 謎紛芥索紀 (Large), Time: 332 ms, Memory: 38212 KB -BaekjoonHub

---
+++ 백준/Silver/14731. 謎紛芥索紀 （Large）/謎紛芥索紀 （Large）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final long MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long answer = 0;
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            long c = Long.parseLong(st.nextToken());
+            long k = Long.parseLong(st.nextToken());
+            answer = (answer + (c * k % MOD) * pow(k - 1)) % MOD;
+        }
+        System.out.println(answer);
+    }
+
+    private static long pow(long expo) {
+        if (expo == -1) return 0;
+        long answer = 1, base = 2;
+        while (expo != 0) {
+            if (expo % 2 != 0) {
+                answer = answer * base % MOD;
+                expo--;
+            }
+            expo >>= 1;
+            base = base * base % MOD;
+        }
+        return answer;
+    }
+}
