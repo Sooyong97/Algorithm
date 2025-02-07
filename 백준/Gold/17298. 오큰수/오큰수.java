4b619c31038ba1e318c921ddb93eba0f672b0789
SOO <122779637+Sooyong97@users.noreply.github.com>
2025-02-07 10:36:20 +0900

    [Gold IV] Title: 오큰수, Time: 924 ms, Memory: 143144 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+        int[] result = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = N - 1; i >= 0; i--) {
+            int num = nums[i];
+
+            while (!stack.isEmpty() && stack.peek() <= num) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+
+            stack.push(num);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(result[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
