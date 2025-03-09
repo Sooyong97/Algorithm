87322f3cc9073fdef4cf4071550431c61e904c34
SOO <122779637+Sooyong97@users.noreply.github.com>
2025-03-09 18:20:16 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        while (st.hasMoreTokens()) {
+            sum += (int) Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+        System.out.println(sum % 10);
+    }
+}
