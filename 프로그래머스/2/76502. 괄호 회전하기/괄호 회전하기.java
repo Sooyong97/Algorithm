04f88858f93ad4430d97a4892e24ada961161d65
SOO <122779637+Sooyong97@users.noreply.github.com>
2025-05-22 16:16:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 22.55 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,31 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
+        int n = s.length();
         int answer = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            String moved = s.substring(i) + s.substring(0, i);
-            if (isAble(moved)) answer++;
+
+        for (int i = 0; i < n; i++) {
+            if (isValid(s, i)) {
+                answer++;
+            }
         }
-        
+
         return answer;
     }
-    
-    static boolean isAble(String s) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+
+    private boolean isValid(String s, int offset) {
+        Deque<Character> stack = new ArrayDeque<>();
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt((i + offset) % n);
+
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } else {
                 if (stack.isEmpty()) return false;
                 char p = stack.pop();
-                if (c == ')' && p != '(') return false;
-                if (c == '}' && p != '{') return false;
-                if (c == ']' && p != '[') return false;
+                if ((c == ')' && p != '(') ||
+                    (c == '}' && p != '{') ||
+                    (c == ']' && p != '[')) {
+                    return false;
+                }
             }
         }
+
         return stack.isEmpty();
     }
 }
