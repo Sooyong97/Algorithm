d723af15779fce6e347ebe8cb2aa5522e388a6e7
SOO <122779637+Sooyong97@users.noreply.github.com>
2025-08-18 15:44:16 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 3.57 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        return n / gcd(6, n);
+    }
+    
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+}
